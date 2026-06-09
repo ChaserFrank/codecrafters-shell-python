aa1680ee62f72f9c75814f9cfd6b7fa16b402f1b
Chaserfrank <wangangafrancis063@gmail.com>
2026-06-09 17:58:34 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,6 +5,12 @@ def main():
     sys.stdout.write("$ ")
     pass
 
+    # Captures the user's command in the "command" variable
+    command = input()
+
+    # Prints the "<command>: command not found" message
+    print(f"{command}: command not found")
+
 
 if __name__ == "__main__":
     main()
