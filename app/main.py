17dde4ceb96a016457f3ded88a61a15475bbd560
Chaserfrank <wangangafrancis063@gmail.com>
2026-06-09 18:20:47 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -10,13 +10,13 @@ def main():
         # Captures the user's command in the "command" variable
         command = input()
 
-        # Prints the "<command>: command not found" message
-        print(f"{command}: command not found")
-
         # Exit the shell
         if command == "exit":
             break
-                      
+
+        # Prints the "<command>: command not found" message
+        print(f"{command}: command not found")
+
 
 if __name__ == "__main__":
     main()
