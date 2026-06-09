20416148383ce5803e0ff975947c4e8dba7f6ed9
Chaserfrank <wangangafrancis063@gmail.com>
2026-06-09 18:11:55 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,15 +2,16 @@ import sys
 
 
 def main():
-    sys.stdout.write("$ ")
-    pass
+    while True:
+        # Display prompt
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
 
-    # Captures the user's command in the "command" variable
-    command = input()
+        # Captures the user's command in the "command" variable
+        command = input()
 
-    # Prints the "<command>: command not found" message
-    print(f"{command}: command not found")
-
+        # Prints the "<command>: command not found" message
+        print(f"{command}: command not found")
 
 if __name__ == "__main__":
     main()
