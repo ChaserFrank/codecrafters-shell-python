1e2c91e732277871fb5a1b561dce5f3d991a71fb
Chaserfrank <wangangafrancis063@gmail.com>
2026-06-15 12:09:45 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ import subprocess
 import shlex
 import readline
 
-BUILTINS = {"echo", "exit", "type", "pwd", "cd", "complete", "jobs", "history"}
+BUILTINS = {"echo", "exit", "type", "pwd", "cd", "complete", "jobs", "history", "declare"}
 BUILTIN_COMPLETIONS = sorted(BUILTINS)
 
 COMPLETIONS = {}
